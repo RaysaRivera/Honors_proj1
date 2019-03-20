28aacfae4a98de887d05993498d8e36441a16df5
RaysaRivera <43319330+RaysaRivera@users.noreply.github.com>
2019-03-20 12:28:21 -0400

    Update Websql_j.js

--- Lesson/Websql_j.js
+++ Lesson/Websql_j.js
@@ -47,9 +47,9 @@ function outputInfo(){
         mydb.transaction(function (t) {
           var username = document.getElementById("inusername").value;
           var password = document.getElementById("inpassword").value;
-          //  t.executeSql("SELECT * FROM users", [], updateInfoList);
+            t.executeSql("SELECT * FROM users", [], updateInfoList);
           var query = "SELECT * FROM users WHERE username = '" + username + "' AND password = '" + password + "'";
-            t.executeSql(query, [], updateInfoList);
+          //  t.executeSql(query, [], updateInfoList);
           //  t.executeSql("SELECT * FROM users WHERE username = '" + username + "' OR 1 = 1", [], updateInfoList);
 
         });
